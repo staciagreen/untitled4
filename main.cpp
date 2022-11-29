46114ea58abbb7ee8f9f93937478c3a0c256262a
Stacia <staciagreen.1@mail.ru>
2022-11-29 16:25:47 +0300

    1.4

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, Bitches!" << std::endl;
     std::cout << "hi" << std::endl;
+    std::cout << "hey there" << std::endl;
     return 0;
 }
